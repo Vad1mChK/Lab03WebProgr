e9bb2de9eea56de6ace5c768c6bd38bfcd1e652f
vadim <snusmumrike2@gmail.com>
2023-10-26 21:09:51 +0300

    misc

---
+++ src/main/java/org/vad1mchk/webprogr/lab03/security/Hasher.java
@@ -0,0 +1,33 @@
+package org.vad1mchk.webprogr.lab03.security;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Represents a hashing mechanism for hashing strings with optional salt values.
+ */
+public interface Hasher {
+    /**
+     * Hashes a given string without an additional salt.
+     *
+     * @param string The string to be hashed.
+     * @return The hashed byte array.
+     */
+    byte[] hash(String string);
+
+    /**
+     * Hashes a given string with an additional salt.
+     *
+     * @param string The string to be hashed.
+     * @param salt The salt byte array to use in the hashing process.
+     * @return The hashed byte array.
+     */
+    byte[] hash(String string, byte[] salt);
+
+    /**
+     * Generates a salt that can be used for hashing.
+     *
+     * @return A byte array representing the generated salt.
+     */
+    byte[] generateSalt();
+}
