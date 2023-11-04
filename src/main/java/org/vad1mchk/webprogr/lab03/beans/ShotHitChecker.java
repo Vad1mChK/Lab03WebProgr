15fcc5c885208c4c8df0b9087dfebc1377b64d90
vadim <snusmumrike2@gmail.com>
2023-11-04 21:45:12 +0300

    misc

--- src/main/java/org/vad1mchk/webprogr/lab03/beans/ShotHitChecker.java
+++ src/main/java/org/vad1mchk/webprogr/lab03/beans/ShotHitChecker.java
@@ -5,23 +5,32 @@ import java.math.RoundingMode;
 
 public class ShotHitChecker {
     public static boolean check(BigDecimal x, BigDecimal y, BigDecimal r) {
-        BigDecimal two = new BigDecimal(2);
-        BigDecimal halfR = r.divide(two, r.scale() + 1, RoundingMode.HALF_UP);
         int xSign = x.signum();
         int ySign = y.signum();
 
-        if (ySign >= 0) {
-            if (xSign >= 0) { // 1st quadrant
-                return false;
-            } else {
-                return x.compareTo(halfR.negate()) >= 0 && y.compareTo(r) <= 0;
-            }
-        } else {
-            if (xSign <= 0) {
-                return x.subtract(y).compareTo(r) <= 0;
-            } else {
-                return x.pow(2).add(y.pow(2)).compareTo(halfR.pow(2)) <= 0;
-            }
-        }
+        return (ySign >= 0 && xSign >= 0 && checkTopRightQuadrant(x, y, r)) ||
+                (ySign >= 0 && xSign <= 0 && checkTopLeftQuadrant(x, y, r)) ||
+                (ySign <= 0 && xSign <= 0 && checkBottomLeftQuadrant(x, y, r)) ||
+                (ySign <= 0 && xSign >= 0 && checkBottomRightQuadrant(x, y, r));
+    }
+
+    private static boolean checkTopRightQuadrant(BigDecimal x, BigDecimal y, BigDecimal r) {
+        return false; // It's a FEATURE
+    }
+
+    private static boolean checkTopLeftQuadrant(BigDecimal x, BigDecimal y, BigDecimal r) {
+        BigDecimal halfR = r.setScale(r.scale() + 1, RoundingMode.HALF_UP)
+                .divide(new BigDecimal(2), RoundingMode.HALF_UP);
+        return x.compareTo(halfR.negate()) >= 0 && y.compareTo(r) <= 0;
+    }
+
+    private static boolean checkBottomLeftQuadrant(BigDecimal x, BigDecimal y, BigDecimal r) {
+        BigDecimal halfR = r.setScale(r.scale() + 1, RoundingMode.HALF_UP)
+                .divide(new BigDecimal(2), RoundingMode.HALF_UP);
+        return x.multiply(x).add(y.multiply(y)).compareTo(halfR.multiply(halfR)) <= 0;
+    }
+
+    private static boolean checkBottomRightQuadrant(BigDecimal x, BigDecimal y, BigDecimal r) {
+        return x.subtract(y).compareTo(r) <= 0;
     }
 }
