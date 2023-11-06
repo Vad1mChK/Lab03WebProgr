9dd98b02146582cae56b7cf7a60ea356337a15c0
vadim <snusmumrike2@gmail.com>
2023-11-07 00:13:25 +0300

    misc

--- src/main/java/org/vad1mchk/webprogr/lab03/beans/TimeZoneBean.java
+++ src/main/java/org/vad1mchk/webprogr/lab03/beans/TimeZoneBean.java
@@ -4,14 +4,27 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.time.ZoneOffset;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @SessionScoped
 @Named("zoneBean")
 public class TimeZoneBean implements Serializable {
+    private static final Map<String, Integer> zones;
     private int offsetSeconds;
     private ZoneOffset zone;
+
+    static {
+        zones = Collections.unmodifiableMap(new LinkedHashMap<>() {{
+            put("Washington, D.C. (UTC-5)", ZoneOffset.ofHours(-5).getTotalSeconds());
+            put("Москва (UTC+3)", ZoneOffset.ofHours(3).getTotalSeconds());
+            put("北京 (UTC+8)", ZoneOffset.ofHours(8).getTotalSeconds());
+            put("東京 (UTC+9)", ZoneOffset.ofHours(9).getTotalSeconds());
+        }});
+    }
+
+    public Map<String, Integer> getZones() {
+        return zones;
+    }
 
     public int getOffsetSeconds() {
         return offsetSeconds;
